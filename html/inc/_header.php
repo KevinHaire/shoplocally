82ebfec96d2f4af284870c184a916c4163308574
KevinHaire <kevinhaire_@hotmail.com>
2014-02-12 19:06:37 -0500

    added a couple images and header into include

---
+++ html/inc/_header.php
@@ -0,0 +1,22 @@
+	<header class="mainHeader">
+		<div class="contain noPadding">
+			<div class="logo">
+				<img class="badgeLogo" src="images/localBadge.png" alt="ShopLocally.com Badge">
+				<img class="fullLogo" src="images/logo.png" alt="ShopLocally.com Logo" />
+			</div>
+			<div class="burgWrapper">
+			  <div class="burg"></div>
+			</div>
+			<nav id="mainNav" class="mainNav">
+				<ul>
+					<li class="on"><a href="">Home</a></li>
+					<li><a href="">Our Story</a></li>
+					<li><a href="">Advocacy</a></li>
+					<li><a href="">Tools</a></li>
+					<li><a href="">Partner</a></li>
+					<li><a href="">Resources</a></li>
+					<li><a href="">Contact</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
